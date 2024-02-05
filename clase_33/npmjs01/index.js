e12f230598238b15a6ba1cb902b11c70183f0661
Carlos Perren <idux.net@gmail.com>
2024-02-05 16:24:48 -0300

    Ejemplo base publicación módulo npmjs.org

---
+++ clase_33/npmjs01/index.js
@@ -0,0 +1,13 @@
+/**
+ * https://www.freecodecamp.org/news/how-to-create-and-publish-your-first-npm-package/
+ */
+
+const sumar = (a, b) => a + b;
+
+const restar = (a, b) => a - b;
+
+const multiplicar = (a, b) => a * b;
+
+const dividir = (a, b) => a / b;
+
+export { sumar, restar, multiplicar, dividir };
