39b09571d42602d4752095b9bafac0f2d0b610ba
Carlos Perren <idux.net@gmail.com>
2024-02-28 13:59:18 -0300

    Comentarios JSDoc y docs generados con swagger

--- clase_39/adoptme/src/app.js
+++ clase_39/adoptme/src/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+// Importamos swaggerJsdoc y swaggerUiExpress
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUiExpress from 'swagger-ui-express';
 
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
@@ -11,6 +14,19 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const connection = mongoose.connect('mongodb://127.0.0.1:27017/coder55605');
 
+// Generamos la configuración inicial para swaggerJsdoc
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.1',
+        info: {
+            title: 'Documentación sistema AdoptMe',
+            description: 'Esta documentación cubre toda la API habilitada para AdoptMe',
+        },
+    },
+    apis: ['./src/docs/**/*.yaml'], // todos los archivos de configuración de rutas estarán aquí
+};
+const specs = swaggerJsdoc(swaggerOptions);
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
@@ -19,5 +35,9 @@ app.use('/api/pets', petsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/sessions', sessionsRouter);
 app.use('/api/adoptions', adoptionsRouter);
+// Inyectamos Swagger como middleware para que genere y sirva la documentación en este endpoint
+// Algo muy cómodo que nos permite Swagger, es no solo documentar las opciones de cada endpoint,
+// sino también poder probarlo allí mismo en la lista de documentación (tryout).
+app.use('/api/docs', swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
